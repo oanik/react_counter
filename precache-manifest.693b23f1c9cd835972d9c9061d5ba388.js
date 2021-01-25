c014009807bfc3526d2d4f5b58a1fa05caa9e334
Olga Rubalska <olga.anikeieva@gmail.com>
2021-01-25 21:49:10 +0200

    Updates

---
+++ precache-manifest.693b23f1c9cd835972d9c9061d5ba388.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95b0bda17f71775131397cca609796d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e761a1184195c676e422",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "7c7a19eaf806aa4e071d",
+    "url": "./static/js/2.cad20d42.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cad20d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e761a1184195c676e422",
+    "url": "./static/js/main.321597bb.chunk.js"
+  },
+  {
+    "revision": "2a286f7172708a7e8c5a",
+    "url": "./static/js/runtime-main.3a2c9fb2.js"
+  }
+]);
